6a94b2fc8891f0b6983c5de8dbaf570ee6608937
Manasses_M_Lira <127964940+manassesmlira@users.noreply.github.com>
2024-04-29 11:07:29 -0300

    api funcionando com mongodb e prisma

---
+++ src/services/CreateCustomerService.ts
@@ -0,0 +1,28 @@
+import prismaClient from "../prisma";
+
+interface CreateCustomerProps{
+    name: string;
+    email: string;
+}
+
+class CreateCustomerService{
+    async execute({name, email}: CreateCustomerProps){
+
+       if(!name || !email) {
+        throw new Error("Preencha todos os campos")
+       }
+
+       const custumer = await prismaClient.customer.create({
+        data:{
+            name,
+            email,
+            status: true
+        }
+       })
+
+        return custumer
+        
+    }
+}
+
+export { CreateCustomerService }
